02b56bb3b603e2f5f83344349d6d33f0bb67fb65
iwarriorgit <warrior_xr@126.com>
2016-10-16 22:08:56 +0800

    commit this web proj

--- src/pack/act.java
+++ src/pack/act.java
@@ -13,8 +13,7 @@ public class act extends ActionSupport{
     private String Name,Country,delete_Isbn,delete_Name;
     private ResultSet resultSet = null;
     private List<item> itemList;
-    private int AuthorID,Age;//
-    private int tempppp=0;
+    private int AuthorID,Age;//ll
 
 
 
